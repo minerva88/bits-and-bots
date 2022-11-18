37ed4b8e61f1552e16247cb41530497774dc8cd3
Minerva88 <linnw88@gmail.com>
2022-11-18 16:31:55 +0100

    React and Dependencies
    Created react app, added sass and bootstrap. Added navbar component for landing-page.

---
+++ src/components/layout/LandingLayout.js
@@ -0,0 +1,9 @@
+import { Navbar } from "react-bootstrap";
+
+export default function LandingLayout() {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="/">BITS & BOTS</Navbar.Brand>
+        </Navbar>
+    )
+}
