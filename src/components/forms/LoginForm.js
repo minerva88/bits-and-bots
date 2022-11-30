4e05a1ce8225d35c18357578da6a935bf2bee7bf
Minerva88 <linnw88@gmail.com>
2022-11-30 18:32:13 +0100

    Login, Forms
    Installed dependencies (react-hook-form, @hookform/resolvers, yup) for form validation. Added environment variables and API-constants. Updated LoginForm.js with async function and validation.

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,22 +1,59 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import axios from 'axios';
+import { BASE_URL, TOKEN_PATH } from '../../constants/api';
 import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
+
+const url = BASE_URL + TOKEN_PATH;
+
+const schema = yup.object().shape({
+    username: yup.string().required("Please enter your username"),
+    password: yup.string().required("Please enter your password"),
+});
 
 
 export default function LoginForm () {
 
+    const [submitting, setSubmitting] = useState(false);
+    const [loginError, setLoginError] = useState(null);
+
+    const { register, handleSubmit, errors } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    async function onSubmit(data) {
+        setSubmitting(true);
+        setLoginError(null);
+
+        console.log(data);
+
+        try {
+            const response = await axios.post(url, data);
+            console.log("response", response.data);
+        } catch (error) {
+            console.log("error", error);
+            setLoginError(error.toString());
+        } finally {
+            setSubmitting(false);
+        }
+    }
+
 
     return (
         <>
-        <Form className='loginform shadow'>
-            <Form.Group className='mb-3' controlId='formEmail'>
-                <Form.Label>Email adress</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' />
+        <Form className='loginform shadow' onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className='mb-3' controlId='formEmail' disabled={submitting}>
+                <Form.Label>Username</Form.Label>
+                <Form.Control type='username' placeholder='Enter username' ref={register} />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='formPassword'>
+            <Form.Group className='mb-3' controlId='formPassword' disabled={submitting}>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Enter Password' />
+                <Form.Control type='password' placeholder='Enter Password' ref={register} />
             </Form.Group>
-            <Button variant='primary' type='submit'>Submit</Button>
+            <Button variant='primary' type='submit'>{submitting ? "Logging in..." : "Login"}</Button>
         </Form>
         </>
     )
