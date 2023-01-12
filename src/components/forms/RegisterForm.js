dc61c395bc17c6916066a5ca3d46d6a00208072d
Minerva88 <linnw88@gmail.com>
2023-01-12 21:22:01 +0100

    RegisterForm
    Updated form. Was not able to create a endpoint for registering new users to my headless CMS on Wordpress.

--- src/components/forms/RegisterForm.js
+++ src/components/forms/RegisterForm.js
@@ -2,19 +2,25 @@ import { Form } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
 
-export default function LoginForm () {
+export default function RegisterForm () {
+
+    //I was not able to find a plugin for my WordPress REST API that allows for user registration.
 
 
     return (
         <>
         <Form className='registerform shadow'>
+            <Form.Group className='mb-3' controlId='registerUsername'>
+                <Form.Label>Username</Form.Label>
+                <Form.Control type='email' placeholder='Enter username' />
+            </Form.Group>
             <Form.Group className='mb-3' controlId='registerEmail'>
                 <Form.Label>Email adress</Form.Label>
                 <Form.Control type='email' placeholder='Enter email' />
             </Form.Group>
             <Form.Group className='mb-3' controlId='registerPassword'>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Enter Password' />
+                <Form.Control type='password' placeholder='Enter password' />
             </Form.Group>
             <Button variant='primary' type='submit'>Submit</Button>
         </Form>
